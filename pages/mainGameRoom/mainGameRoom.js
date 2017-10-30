1b3c48cfd2d24596ae39d864b7d9b0512fc5e61a
EX_KJKFB_XIAOHONGJ <EX_KJKFB_XIAOHONGJ@pingan.com.cn>
2017-10-30 12:31:22 +0800

    更新

--- pages/mainGameRoom/mainGameRoom.js
+++ pages/mainGameRoom/mainGameRoom.js
@@ -2,6 +2,46 @@ var util = require("../../utils/util.js");
 
 var app = getApp();
 Page({
+  /**
+   * 通过data初始化数据
+   */
+  data: {
+  },
+  /**
+   * 监听页面开在加载的状态
+   *    页面加载完成之后就不会在执行
+   */
+  onLoad: function () {
+    
+  },
+  /**
+   *  监听页面显示，
+   *    当从当前页面调转到另一个页面
+   *    另一个页面销毁时会再次执行
+   */
+  onShow: function () {
+    console.log('index---------onShow()')
+  },
+  /**
+   * 监听页面渲染完成
+   *    完成之后不会在执行
+   */
+  onReady: function () {
+    console.log('index---------onReaday()');
+  },
+  /**
+   * 监听页面隐藏
+   *    当前页面调到另一个页面时会执行
+   */
+  onHide: function () {
+    console.log('index---------onHide()')
+  },
+  /**
+   * 当页面销毁时调用
+   */
+  onUnload: function () {
+    console.log('index---------onUnload')
+  }
   // chooseRole: function () {
   //   this.setData({
   //     isChoosing: false,
